df5b38706e4cfcc8f3cfc70446349e7fa0d1f9e1
Hang <yhnbgfd@gmail.com>
2018-11-14 13:43:18 +0800

    Initial project.

---
+++ Hang.NLog.Extension/LoggerEventArgs.cs
@@ -0,0 +1,45 @@
+﻿using NLog;
+using System;
+
+namespace Hang.NLog.Extension
+{
+    /// <summary>
+    /// 日志事件参数
+    /// </summary>
+    public class LoggerEventArgs : EventArgs
+    {
+        /// <summary>
+        /// 时间
+        /// </summary>
+        public DateTime Date => Info.TimeStamp;
+        /// <summary>
+        /// 等级
+        /// </summary>
+        public string Level => Info.Level.ToString();
+        /// <summary>
+        /// 对象名（不带命名空间）
+        /// </summary>
+        public string Logger => Info.LoggerName.Substring(Info.LoggerName.LastIndexOf('.') + 1);
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string Message => Info.Message;
+        /// <summary>
+        /// 格式化后的日志（本地保存时的格式）
+        /// </summary>
+        public string RenderLayout { get; set; }
+        /// <summary>
+        /// NLog.LogEventInfo（原始NLog对象）
+        /// </summary>
+        public LogEventInfo Info { get; set; }
+
+        /// <summary>
+        /// 传入NLog.LogEventInfo的构造函数
+        /// </summary>
+        /// <param name="info"></param>
+        public LoggerEventArgs(LogEventInfo info)
+        {
+            Info = info;
+        }
+    }
+}
---
+++ Hang.NLog.Extension/RaiseEventTarget.cs
@@ -0,0 +1,22 @@
+﻿using NLog;
+using NLog.Targets;
+using System;
+
+namespace Hang.NLog.Extension
+{
+    [Target("RaiseEvent")]
+    public class RaiseEventTarget : TargetWithLayout
+    {
+        public static event EventHandler<LoggerEventArgs> WriteEvent;
+
+        protected override void Write(LogEventInfo logEvent)
+        {
+            var args = new LoggerEventArgs(logEvent)
+            {
+                RenderLayout = Layout.Render(logEvent)
+            };
+
+            WriteEvent?.Invoke(null, args);
+        }
+    }
+}
